e3db75f873638dbb8a0be7b168f75f709bbd0e78
sipwarriper <sipwarriper@gmail.com>
2020-09-14 18:53:14 +0200

    first endpoint

--- html/nodejs/index.js
+++ html/nodejs/index.js
@@ -1,12 +1,26 @@
 'use strict'
+//nodemon --> libreria de desarrollo para que los cambios que se hagan en entorne de desarrollo se vean aplicados sin tener q reiniciar el servidor cada vez
+
 
 const express = require('express') //importa express
 
-//creem el servidor
-const app = express() //crida express
+const bodyParser = require('body-parser'); //body-parser es un framework per parsejar info de crides http... l'usarem com a middlewhere de epxress
 
-app.listen(3000, () =>{
-    console.log('API REST corriendo en http://sipwarriper.com:3000')
+//creem el servidor
+const app = express(); //crida express
+const port = process.env.PORT || 3000; //escoltem port donat per env o usem 3000 per defecte
+
+
+app.use(bodyParser.urlencoded({ extended: false})) //afegim middlewhere
+app.use(bodyParser.json()) //afegim middlewhere
+
+app.get('/',(req, rest)=>{
+    res.send({message: 'Hola mundo!'})
+})
+
+
+app.listen(port, () =>{
+    console.log(`API REST corriendo en http://nodejs.sipwarriper.com:${port}`)
 })
 
 
